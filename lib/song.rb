ac3083424f8ae3f6d9a464066dc6750c7f817280
Chloe Gell <gellert.chloe@gmail.com>
2020-02-18 02:27:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,8 +44,9 @@ class Song
     genre_name = splits[2].split(".")[0]
     new_song = self.find_or_create_by_name(song_name)
     new_artist = Artist.find_or_create_by_name(artist_name)
+    new_genre = Genre.find_or_create_by_name(genre_name)
     new_song.artist=(new_artist)
-    new_song.genre=(genre_name)
+    new_song.genre=(new_genre)
     new_song
     #binding.pry
   end
